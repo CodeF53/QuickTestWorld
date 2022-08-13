27059f91227350018fe356de9c92eda4878a88df
CodeF53 <37855219+CodeF53@users.noreply.github.com>
2022-08-12 21:10:45 -0600

    Add Platform for player to stand.

--- forge/src/main/java/net/testworld/forge/TestWorldForge.java
+++ forge/src/main/java/net/testworld/forge/TestWorldForge.java
@@ -1,6 +1,5 @@
 package net.testworld.forge;
 
-import com.llamalad7.mixinextras.MixinExtrasBootstrap;
 import dev.architectury.platform.forge.EventBuses;
 import net.testworld.TestWorld;
 import net.minecraftforge.fml.common.Mod;
@@ -9,8 +8,6 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 @Mod(TestWorld.MOD_ID)
 public class TestWorldForge {
     public TestWorldForge() {
-        MixinExtrasBootstrap.init();
-
         EventBuses.registerModEventBus(TestWorld.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
         TestWorld.init();
     }
