e57e59503379528fa362d326780d6f66f9e49c92
CodeF53 <37855219+CodeF53@users.noreply.github.com>
2022-08-12 22:46:00 -0600

    yeet arch api

--- forge/src/main/java/net/testworld/forge/TestWorldForge.java
+++ forge/src/main/java/net/testworld/forge/TestWorldForge.java
@@ -1,14 +1,11 @@
 package net.testworld.forge;
 
-import dev.architectury.platform.forge.EventBuses;
 import net.testworld.TestWorld;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(TestWorld.MOD_ID)
 public class TestWorldForge {
     public TestWorldForge() {
-        EventBuses.registerModEventBus(TestWorld.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
         TestWorld.init();
     }
 }
